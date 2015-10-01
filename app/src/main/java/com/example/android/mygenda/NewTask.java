b5f3897bff6b7b27ae1343d12b52740fad0ffc07
Gabriel Cole <lgcw92@gmail.com>
2015-09-30 20:21:19 -0500

    making the new task layout and saving tasks in the back-end

--- app/src/main/java/com/example/android/mygenda/NewTask.java
+++ app/src/main/java/com/example/android/mygenda/NewTask.java
@@ -1,37 +1,66 @@
 package com.example.android.mygenda;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
 
 public class NewTask extends AppCompatActivity {
 
+    EditText mTitleEditText;
+    EditText mDescriptionEditText;
+    ProgressBar mProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
+
+        mTitleEditText = (EditText)findViewById(R.id.title_task);
+        mDescriptionEditText = (EditText)findViewById(R.id.description_task);
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_new_task, menu);
-        return true;
-    }
+    public void saveTask(View view){
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        mProgressBar.setVisibility(View.VISIBLE);
+        String taskTitle = mTitleEditText.getText().toString();
+        String taskDescription = mDescriptionEditText.getText().toString();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        ParseObject task = new ParseObject(ParseConstants.CLASS_TITLE);
+        task.put(ParseConstants.KEY_TASK_TITLE, taskTitle);
+        task.put(ParseConstants.KEY_TASK_DESCRIPTION, taskDescription);
+        task.saveEventually(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    //Done
+                    mProgressBar.setVisibility(View.INVISIBLE);
 
-        return super.onOptionsItemSelected(item);
+                    Intent intent = new Intent(NewTask.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+
+                }
+                else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(NewTask.this);
+                    builder.setMessage(e.getMessage());
+                    builder.setTitle("Error");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/android/mygenda/ParseConstants.java
@@ -0,0 +1,13 @@
+package com.example.android.mygenda;
+
+/**
+ * Created by Gabriel on 09/30/15.
+ */
+public class ParseConstants {
+
+    public static final String CLASS_TITLE = "Task";
+
+    public static final String KEY_TASK_TITLE = "title";
+    public static final String KEY_TASK_DESCRIPTION = "description";
+    public static final String KEY_IMPORTANCE = "importance";
+}
